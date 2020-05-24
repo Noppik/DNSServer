ad7a76c81720c1f49a8256f6a3d4d053b4afaf38
Noppik <55311174+Noppik@users.noreply.github.com>
2020-05-24 16:50:06 +0500

    Update testdns.py

--- testdns.py
+++ testdns.py
@@ -1,7 +1,6 @@
 import socket
 import dnslib
 
-socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-data = dnslib.DNSRecord.question("vk.com")
-socket.sendto(data.pack(), ('localhost', 53))
-socket.close()
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server.sendto(dnslib.DNSRecord.question("vk.com").pack(), ('localhost', 53))
+server.close()
